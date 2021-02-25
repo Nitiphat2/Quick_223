db05d3b7717fe24a43d6a88b262f23d35744e5f4
Nitiphat2 <moozknot@gmail.com>
2021-02-25 16:30:13 +0700

    1

--- 6320502410_2.c
+++ 6320502410_2.c
@@ -1,13 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j=0;
     scanf("%d",&n);
-    int A[n],B[n];
-    for(i=0;i<n;i++)
+    int A[n],B[n],c[n];
+    for(i=0; i<n; i++)
     {
         scanf("%d",&A[i]);
         scanf("%d",&B[i]);
+        c[i]=0;
     }
+    for(i=0; i<n; i++)
+    {
+        if(i==A[i])
+        {
+            if((B[i]>0)&&(B[i]<=5))
+            {
+                c[i]=c[i]+B[i];
+            }
+        }
+    }
+    int max[n];
+    max[0]=c[0];
+    for(i=0; i<n; i++)
+    {
+        if(max[j] < c[i])
+        {
+            max[j] =c[i]+1;
+        }
+    }
+    printf("%d",max[0]);
 
 }
